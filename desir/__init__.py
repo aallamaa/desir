bb7ed4eda4a465be3e62bc33a38e4928b99c2587
Abdelkader ALLAM <abdelkaderallam@MacBook-Pro-de-Abdelkader.local>
2012-06-16 22:45:31 +0000

    Making RedisInner "importable..."

--- desir/__init__.py
+++ desir/__init__.py
@@ -1,2 +1,2 @@
 #from desir import *
-from .desir import SubAsync,Node,Redis,RedisError,NodeError,ConnectorError,SWM
+from .desir import SubAsync,Node,Redis,RedisError,NodeError,ConnectorError,SWM,RedisInner
